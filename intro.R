ed0a8e30b72b7d39355e1c770210f0caca37d57e
Michelle Chiu <michelle.chiu@temple.edu>
2021-07-15 16:38:28 -0400

    cleaning rmarkdown file

--- intro.R
+++ intro.R
@@ -1,32 +1,37 @@
-# ggplot2 is in the tidyverse suite of packages
+#### ggplot2 is in tidyverse suite of pkgs ####
 library(tidyverse)
-data("economics")
-# ?economics  # uncomment and run to read about dataset
+# ?economics  # uncomment and run for dataset info
+data("economics") # built-in dataset in base R
+
+# assign base layer to object, not required but keeps code cleaner
 econ <- ggplot(economics, aes(x = date, y = pop))
 econ + geom_line # diff geometries
 econ + geom_point
 
 # aes, or aesthetics
+# ?mtcars # uncomment, run for dataset info (also built-in base R)
 ## data-driven variables go inside aes
 ggplot(mtcars, aes(x = hp, y = mpg)) + 
   geom_point(aes(colour = factor(cyl), size = gear))
-
-## outside aes
+## fixed values go outside aes
 ggplot(mtcars, aes(x = hp, y = mpg)) + 
   geom_point(colour = "violet", size = 3) # inherits aes from ggplot
 
-# choroplethr and choroplethrMaps
+#### choroplethr and choroplethrMaps ####
 library(choroplethr)
 library(choroplethrMaps)
+?df_state_demographics
 data("df_state_demographics")
 ## what happens if we try to plot directly?
 state_choropleth(df_state_demographics)
 ## set 'value' column to be the column of interest, then plot again
+# note default parameters
 df_state_demographics$value = df_state_demographics$median_age
-state_choropleth(df_state_demographics,  # default parameters, title/legend
+state_choropleth(df_state_demographics,
                  title = "US 2013 - 2017 State Median Age",
                  legend = "Median Age (years)")
-## continuous scale and zoom
+
+## customize: continuous scale (num_colors), zoom in on vector of states
 state_choropleth(df_state_demographics,
                  title = "US 2013 - 2017 State Median Age",
                  legend = "Median Age (years)",
